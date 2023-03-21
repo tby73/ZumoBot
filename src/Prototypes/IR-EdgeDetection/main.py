e52fc3e9c77cdbf5b5cda07d46aa23779acf362b
poldeyby <83162717+tby73@users.noreply.github.com>
2023-03-21 16:10:50 +0100

    Update IR-Sensor interfacing

--- src/Prototypes/IR-EdgeDetection/main.py
+++ src/Prototypes/IR-EdgeDetection/main.py
@@ -4,9 +4,6 @@ import time
 # DATA_OUT => GPIO 14
 IR_SENSOR_DOUT = 14
 
-# Check DO from IR-Sensor every 10 ms
-CHECK_TIME = 0.01
-
 def InitSystem():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(IR_SENSOR_DOUT, GPIO.OUT)
@@ -17,9 +14,9 @@ def main():
     while True:
         # read sensor state
         if GPIO.input(IR_SENSOR_DOUT):
-            print(f"Edge detected, avoid")
+            print("Edge detected, avoid")
         else:
-            continue
+            print("Inside marked space, continue")
 
 if __name__ == "__main__":
     main()
