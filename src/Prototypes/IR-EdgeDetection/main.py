62a39eea16276bddac5ec89628ad699ca93c028c
poldeyby <83162717+tby73@users.noreply.github.com>
2023-03-21 16:09:14 +0100

    Updated IR-Sensor interfacing

--- src/Prototypes/IR-EdgeDetection/main.py
+++ src/Prototypes/IR-EdgeDetection/main.py
@@ -1,6 +1,5 @@
 import RPi.GPIO as GPIO
 import time
-import numpy as np
 
 # DATA_OUT => GPIO 14
 IR_SENSOR_DOUT = 14
@@ -13,23 +12,14 @@ def InitSystem():
     GPIO.setup(IR_SENSOR_DOUT, GPIO.OUT)
 
 def main():
-    last_state = 0
-    current_state = 1
+    InitSystem()
     
     while True:
         # read sensor state
-        current_state = GPIO.input(IR_SENSOR_DOUT)
-
-        if current_state != last_state:
-
-            # edge marking detected
-            if current_state == 1:
-                print("Edge detected, avoid")
-            
-            last_state = current_state
-        
-        # wait and check again
-        time.sleep(CHECK_TIME)
+        if GPIO.input(IR_SENSOR_DOUT):
+            print(f"Edge detected, avoid")
+        else:
+            continue
 
 if __name__ == "__main__":
     main()
