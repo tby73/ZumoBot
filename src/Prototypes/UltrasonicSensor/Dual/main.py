ae6b6a57568197b427c18da3ea3e9c86d9358281
Oliver Pauer <83162717+tby73@users.noreply.github.com>
2023-03-28 12:53:38 +0200

    Updated measurement visualization

--- src/Prototypes/UltrasonicSensor/Dual/main.py
+++ src/Prototypes/UltrasonicSensor/Dual/main.py
@@ -52,11 +52,13 @@ def GetDistanceCM(trigger, echo):
     return distance
 
 def PlotDistance(distance1, distance2):
+    # setup plot figure
     fig, ax = plt.subplots()
 
     ax.set_xlim(0, 2 * np.pi)
     ax.set_ylim(-1, 1)
 
+    # create line objects for both distances
     line_dist1, = ax.plot([], [], label="Sensor 1")
     line_dist2, = ax.plot([], [], label="Sensor 2")
 
@@ -65,14 +67,18 @@ def PlotDistance(distance1, distance2):
     def UpdatePlot(frame):
         x = np.linspace(0, 2 * np.pi, 1000)
 
+        # plot data
         line_dist1.set_data(x, distance1)
         line_dist2.set_data(x, distance2)
 
         return line_dist1, line_dist2
 
+    # init animation
     animation = FuncAnimation(fig, UpdatePlot, frames=np.linspace(0, 2 * np.pi, 200), blit=True, interval=10)
 
+    # display
     plt.show()
+
 
 
 def main():
